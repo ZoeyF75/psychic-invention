9d77a065aecfbf71707569354a273f88083b052b
ZoeyF75 <zf.zoodle@gmail.com>
2021-07-19 17:08:56 -0700

    git restore

---
+++ weather-app-react-native/App.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionAsync();
+      if (status !== 'granted') {
+        setErrorMessage('Access to location is needed to run the app');
+        return
+      }
+    } 
+    catch (error) {
+
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello from react native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+import * as Location from 'expo-location';
+
+import * as Location from 'expo-location';
+
