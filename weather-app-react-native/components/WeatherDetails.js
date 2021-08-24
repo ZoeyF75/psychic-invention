50f900d4d819278566955b89e05e9eec19ef1614
ZoeyF75 <zf.zoodle@gmail.com>
2021-08-24 16:30:03 -0700

    humidity and feels like bug, doesnt display in stylesDetailsBox

--- weather-app-react-native/components/WeatherDetails.js
+++ weather-app-react-native/components/WeatherDetails.js
@@ -5,11 +5,14 @@ import { colors } from '../utils/index';
 const {PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR} = colors;
 
 export default function WeatherDetails({ currentWeather }) {
-  const { //deconstructing currentWeather obj
+  const {
     main: { feels_like, humidity }
   } = currentWeather;
 
+  console.log(feels_like, humidity);
+
   return (
+    
     <View style={styles.weatherDetails}>
       <View style={styles.WeatherDetailsRow}>
         <View style={styles.weatherDetailsBox}>
@@ -25,19 +28,29 @@ export default function WeatherDetails({ currentWeather }) {
 
 const styles = StyleSheet.create({
   weatherDetails: {
-    marginTop: 'auto',
-    margin: 15,
-    borderWidth: 1,
-    borderColor: BORDER_COLOR,
-    borderRadius: 10,
+      marginTop: 'auto',
+      margin: 15,
+      borderWidth: 1,
+      borderColor: BORDER_COLOR,
+      borderRadius: 10,
   },
-  weatherDetailsRow: { //everything in reactnative is using flex property
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
+  weatherDetailsRow: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
   },
   weatherDetailsBox: {
-    flex: 1,
-    padding: 20
-  }
+      flex: 1,
+      padding: 20,
+  },
+  weatherDetailsItems: {
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
+  },
+  textSecondary: {
+      fontSize: 15,
+      color: SECONDARY_COLOR,
+      fontWeight: '700',
+      margin: 7,
+  },
 });
